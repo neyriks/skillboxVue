631543afc70196943b478cf51a9e43baa8b4f2a8
Neyriks <mesuppgg@gmail.com>
2021-08-20 16:39:05 +0300

    Lesson 3.6

---
+++ src/data/products.js
@@ -0,0 +1,63 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB ',
+    price: 21790,
+    image: '/img/phone.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+];
